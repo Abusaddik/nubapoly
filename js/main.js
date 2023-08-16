722eb302715f6b5e50070cfdab296c0db7a1b1e5
Abusaddik <90425976+Abusaddik@users.noreply.github.com>
2023-08-16 12:24:47 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+
+    $(".text").typed({
+        strings:["Welcome To","Data View"],
+        typespeed:0,
+
+    });
+
+    "use restrict"
+        $('.slider').ripples({
+            dropRadius: 15,
+            perturbance: 0.01,
+
+        });
+
+
+});
+
+
+
+document.write(new Date().getFullYear());
